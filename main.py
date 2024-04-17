f88e0261d2036b5f034e4122726e0f695e33703a
Youssef Najjarine <youssefnajjarine@hotmail.com>
2024-04-17 15:11:23 -0500

    Completed day-1.

--- main.py
+++ main.py
@@ -68,4 +68,5 @@ name = 'Johnny'
 age = 55
 print('hi ' + name + '. You are ' + str(age) +  ' years old')
 print('hi {}. You are {} years old'.format('Johnny', '55'))
+
 print(f'hi {name}. You are {age} years old')
