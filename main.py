0acf86ee2eb0360ef42c83098267d7a268e44210
Youssef Najjarine <youssefnajjarine@hotmail.com>
2024-04-26 20:22:45 -0500

    Completed day 10.

--- main.py
+++ main.py
@@ -257,3 +257,75 @@ print(my_set)
 #.issubset()
 #.issuperset()
 #.union()
+
+
+class User():
+  def sign_in(self):
+    print('logged in')
+
+
+class Wizard(User):
+  def __init__(self,name,power):
+    self.name = name
+    self.power = power
+
+  def attack(self):
+    print(f'attacking with power of {self.power}')
+
+
+class Archer(User):
+  def __init__(self,name,num_arrows):
+    self.name = name
+    self.num_arrows = num_arrows
+
+  def attack(self):
+    print(f'attacking with arrows: arrows left {self.num_arrows}')
+
+
+wizard1 = Wizard('Merlin', 50)
+archer1 = Archer('Robin', 100)
+
+wizard1.attack()
+archer1.attack()
+
+print(isinstance(wizard1, Wizard))
+print(isinstance(wizard1, User))
+print(wizard1.sign_in())
+
+
+class Pets():
+  animals = []
+  def __init__(self, animals):
+      self.animals = animals
+
+  def walk(self):
+      for animal in self.animals:
+          print(animal.walk())
+
+class Cat():
+  is_lazy = True
+
+  def __init__(self, name, age):
+      self.name = name
+      self.age = age
+
+  def walk(self):
+      return f'{self.name} is just walking around'
+
+
+class Simon(Cat):
+  def sing(self, sounds):
+      return f'{sounds}'
+
+class Sally(Cat):
+  def sing(self, sounds):
+      return f'{sounds}'
+
+#1 Add nother Cat
+class Coonster(Cat):
+  def sing(self, sounds):
+      return f'{sounds}'
+my_cats = []
+my_cats.extend((Simon('Simon',5),Sally('Sally',6),Coonster('Coonster',7)))
+
+Pets(my_cats).walk()
